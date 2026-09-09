ce682f6e64e9c764750b73483b46856c3d2a2e45
Thomas Titz Mite <156305897+TitzMite@users.noreply.github.com>
2026-09-09 23:37:20 +0200

    Add files via upload

--- grp_CSA2.py
+++ grp_CSA2.py
@@ -191,3 +191,100 @@ class A2Word:
 
 gens = [A2Word([i]) for i in [-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7]]
 
+#########
+
+a1 = A2Word([-3])
+a2 = A2Word([2])
+a3 = A2Word([3])
+a4 = A2Word([7])
+a5 = A2Word([-6, -5])
+a6 = A2Word([-4, -2])
+a7 = A2Word([-3, -5])
+a8 = A2Word([-3, -2])
+a9 = A2Word([1, -4])
+a10 = A2Word([2, -7])
+a11 = A2Word([2, -5])
+a12 = A2Word([2, -1])
+a13 = A2Word([2, 4])
+a14 = A2Word([2, 7])
+a15 = A2Word([3, -7])
+a16 = A2Word([3, -6])
+a17 = A2Word([3, -2])
+a18 = A2Word([3, 6])
+a19 = A2Word([7, -5])
+a20 = A2Word([7, -2])
+a21 = A2Word([7, 6])
+a22 = A2Word([7, 7])
+a23 = A2Word([-4, -2, -2])
+a24 = A2Word([1, -4, -6])
+a25 = A2Word([2, -7, -2])
+a26 = A2Word([2, -1, -6])
+a27 = A2Word([2, 4, -6])
+a28 = A2Word([2, 4, -2])
+a29 = A2Word([2, 4, 6])
+a30 = A2Word([2, 7, -6])
+a31 = A2Word([3, -6, -5])
+a32 = A2Word([3, -2, -2])
+a33 = A2Word([3, 2, -1])
+a34 = A2Word([3, 6, -2])
+a35 = A2Word([7, -2, -2])
+a36 = A2Word([7, 6, -2])
+a37 = A2Word([7, 7, -6])
+a38 = A2Word([3, 2, -1, -6])
+
+
+b1 = A2Word([])
+b2 = A2Word([-6])
+b3 = A2Word([-4])
+b4 = A2Word([-3])
+b5 = A2Word([-2])
+b6 = A2Word([-1])
+b7 = A2Word([1])
+b8 = A2Word([3])
+b9 = A2Word([4])
+b10 = A2Word([6])
+b11 = A2Word([-7, -4])
+b12 = A2Word([-7, -1])
+b13 = A2Word([-6, -3])
+b14 = A2Word([-2, -1])
+b15 = A2Word([2, -3])
+b16 = A2Word([3, -4])
+b17 = A2Word([3, -1])
+b18 = A2Word([4, -1])
+b19 = A2Word([6, -4])
+b20 = A2Word([6, -1])
+b21 = A2Word([7, -3])
+b22 = A2Word([7, -2])
+b23 = A2Word([7, 3])
+b24 = A2Word([7, 7])
+b25 = A2Word([-6, -7, -4])
+b26 = A2Word([-6, -7, -1])
+b27 = A2Word([1, -7, -4])
+b28 = A2Word([1, -7, -1])
+b29 = A2Word([1, -6, -3])
+b30 = A2Word([3, 2, -3])
+b31 = A2Word([6, 3, -4])
+b32 = A2Word([6, 3, -1])
+b33 = A2Word([7, -2, -1])
+b34 = A2Word([7, 7, 3])
+b35 = A2Word([1, 1, -6, -3])
+b36 = A2Word([6, -4, -6, -3])
+b37 = A2Word([6, 3, 2, -3])
+
+A = [
+    a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
+    a11, a12, a13, a14, a15, a16, a17, a18, a19, a20,
+    a21, a22, a23, a24, a25, a26, a27, a28, a29, a30,
+    a31, a32, a33, a34, a35, a36, a37, a38,
+]
+
+B = [
+    b1, b2, b3, b4, b5, b6, b7, b8, b9, b10,
+    b11, b12, b13, b14, b15, b16, b17, b18, b19, b20,
+    b21, b22, b23, b24, b25, b26, b27, b28, b29, b30,
+    b31, b32, b33, b34, b35, b36, b37,
+]
+
+# >>> import grp_CSA2 as CSA2
+# >>> CSA2.grp.has_no_unique_product(CSA2.A, CSA2.B)
+# True
